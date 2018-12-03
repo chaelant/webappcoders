bbf6b199c3e3b13c584a2433c79b7173a6d607b5
Michael <mikeramos92@gmail.com>
2018-12-03 11:48:30 +0800

    display db on route /db

--- routes/db.js
+++ routes/db.js
@@ -5,7 +5,6 @@ const dbOps = require('../mongo');
 router.get('/', async(req, res) => {
     try {
         const allBusinesses = await dbOps.getAllBusinesses();
-        console.log(allBusinesses);
         res.json(allBusinesses);
     } catch (e) {
         res.sendStatus(500);
