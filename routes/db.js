bd31ffff9adb364d1aea86aa9c094033e9d0da85
Justin <justin@Justins-MBP.fios-router.home>
2018-12-13 23:31:49 -0500

    html

--- routes/db.js
+++ routes/db.js
@@ -6,7 +6,7 @@ const businesses = data.businesses;
 router.get('/', async(req, res) => {
     try {
         const allBusinesses = await businesses.getAllBusinesses();
-        res.json(allBusinesses);
+        res.render("homepage", {business: allBusinesses});
     } catch (e) {
         res.sendStatus(500);
     }
