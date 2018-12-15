8619eb62b3f5e5d40bab8e2a24c64891effb1664
Michael <mikeramos92@gmail.com>
2018-12-15 22:47:14 +0800

    adjusted createBusiness routes

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,29 @@ const router = express.Router();
 const data = require('../data/');
 const users = data.users;
 
-router.get('/', async(req, res) =>{
-    try {
-        res.sendStatus(200);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+router.get("/createBusiness", (req, res) => {
+    res.render("users/createBusiness");
+});
+
+router.post("/createBusiness", (req, res) => {
+    const buis = {
+        rating: 0, //this gets updated with each added review
+        price: req.body.price,
+        phone: req.body.phone,
+        alias: req.body.name,
+        is_closed: false,
+        categories: [req.body.categories],
+        review_count: 0, //this gets updated with each added review
+        name: req.body.name,
+        coordinates: req.body.address,
+        image_url: req.body.image_url,
+        location: {display_address: req.body.address},
+        transactions: "idk",
+        distance: "idk" //dependent on user location
+    };
+
+    businesses.addBusiness(buis);
+    res.redirect("/");
 });
 
 module.exports = router;
